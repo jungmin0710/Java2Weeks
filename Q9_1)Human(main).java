39a985e4ab36b9fc99f0ce1dfa9bed56c5eb08b5
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-21 17:50:48 +0900

    Create Q9_1)Human(main).java

---
+++ Q9_1)Human(main).java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Human gildong = new Human("길동", 170,60);
+    Human chulsu = new Human("철수", 166,72
+    );
+
+    System.out.println("gildong = " + gildong);
+    System.out.println("chulsu = " + chulsu);
+    
+  }
+}
