a9e4b221ea8379058670e104f0f7155c59d41e8f
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-22 10:16:40 +0900

    Create Q10_1)HumanNum(main).java

---
+++ Q10_1)HumanNum(main).java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    Human gildong = new Human("홍길동",170,60);
+    Human chulsu = new Human("김철수", 166,72);
+
+    gildong.putData();
+    System.out.println("번호 : " + gildong.getId());
+
+    System.out.println();
+
+    chulsu.putData();
+    System.out.println("번호 : " + chulsu.getId());
+
+  }
+}
