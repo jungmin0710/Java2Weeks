d7e571e7fa10f518e70fa334b90c39bbfd6d242c
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-23 18:01:19 +0900

    Create Q13_2)Animal(animal).java

---
+++ Q13_2)Animal(animal).java
@@ -0,0 +1,16 @@
+abstract class Animal{
+
+  private String name;
+  public Animal(String name) { this.name=name;}
+  public abstract void bark();
+  public String getName() {return name;}
+  public void introduce() {
+    System.out.println(toString() + "이다 ");
+    bark();
+  }
+
+
+
+
+
+}
