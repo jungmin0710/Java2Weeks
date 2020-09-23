8fb735f6bf26f0893bbb088a1eb6c9a95a5bdd72
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-23 18:05:29 +0900

    Create Q13_1Animal(cat).java

---
+++ Q13_1Animal(cat).java
@@ -0,0 +1,9 @@
+class Cat extends Animal{
+  private int age; //나이
+  
+  public Cat(String name, int age) {
+    super(name); this.age = age;
+  }
+
+  public void bark() {System.out.println("냥냥!");}
+}
