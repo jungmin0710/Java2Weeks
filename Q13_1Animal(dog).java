0226cf6c7e425914e92a2ccba00ca9914f5ff5cf
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-23 18:05:59 +0900

    Create Q13_1Animal(dog).java

---
+++ Q13_1Animal(dog).java
@@ -0,0 +1,8 @@
+class Dog extends Animal{
+  private String type; //개의 종류
+  public Dog(String name, String type) {
+    super(name); this.type = type;
+  }
+
+  public void bark() {System.out.println("멍멍!");}
+}
