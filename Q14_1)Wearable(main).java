0134a6293d9997a25d643631d29175cd4391cc82
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-24 10:29:47 +0900

    Create Q14_1)Wearable(main).java

---
+++ Q14_1)Wearable(main).java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    
+    
+    Wearable[] a = new Wearable[2];
+    a[0] = new Headphone();
+    a[1] = new WearableComputer();
+
+    for (int i = 0; i < a.length; i++)
+      a[i].putOn();
+
+    for (int i = 0; i < a.length; i++)
+      a[i].putOff();
+  }
+}
