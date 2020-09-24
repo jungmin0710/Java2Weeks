50dec2ed518f53fd18c23e2e580dfee8592256e4
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-24 13:08:04 +0900

    Update Q14_5)HeadUp(HeadMounted).java

--- Q14_5)HeadUp(HeadMounted).java
+++ Q14_5)HeadUp(HeadMounted).java
@@ -1,30 +1,29 @@
-public class HeadMounted implements Wearable, Skinnable {
-  
-  private int skin;
+class Main {
+  public static void main(String[] args) {
 
-  public void putOn() {
-    System.out.println("디스플레이를 착용했습니다.");
+    HeadMounted hmd = new HeadMounted();
+    hmd.putOn();
+    hmd.putOff();
+    hmd.changeSkin(Skinnable.YELLOW);
+    hmd.putSkin();
+
+    Wearable w = hmd;
+    w.putOn();
+    w.putOff();
+
+    Skinnable s = hmd;
+    s.changeSkin(Skinnable.BLACK);
+
+    hmd.putSkin();
+
+
+    
   }
-
-  public void putOff() {
-    System.out.println("디스플레이를 벗었습니다.");
-  }
-
-  public void changeSkin(int skin) {
-    this.skin = skin;
-  }
-
-  public void putSkin() {
-    switch (skin){
-      case BLACK: System.out.println("BLACK DISPLAY"); break;
-      case RED: System.out.println("RED DISPLAY"); break;
-      case GREEN: System.out.println("GREEN DISPLAY"); break;
-      case BLUE: System.out.println("BLUE DISPLAY"); break;
-      case YELLOW: System.out.println("YELLOW DISPLAY"); break;
-    }
-  }
-
 }
+//간략하게 정리(연결고리로 이어진 느낌인듯)
+//인터페이스: 꼭 만들어야 하는 메서드의 틀만 잡아줌(상수 정의 가능, 메서드 자체는 정의 안됨)
+//클래스에서 인터페이스에서 잡혀진 메서드의 틀로 실제 메서드를 만들어야함(꼭)
+//메인에서 클래스의 메서드를 끌어와서 실행시킴
 
-
-//클래스의 파생(상속) 인터페이스 구현과 가장 큰 차이점: 인터페이스는 복수의 구현이 가능하다. 클래스 파생(상속)은 단일 상속만 가능하다. 복수의 친구관계가 성립한다.
+//클래스형 인스턴스
+//HeadMounted 클래스 형의 변수 hmd, Wearable형의 변수 w, Skinnable형의 변수 s는 모두 생성된 동일 인스턴스를 참조한다. 변수 hmd, w,s의 형이 각각 다르기 때문에 해당 형에서 호출 가능한 메서드만 호출한다.
