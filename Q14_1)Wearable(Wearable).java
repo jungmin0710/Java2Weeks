c20aa725cd61c8f509d12f157df2ade9b3ad7255
jungmin0710 <67640843+jungmin0710@users.noreply.github.com>
2020-09-24 10:31:16 +0900

    Create Q14_1)Wearable(Wearable).java

---
+++ Q14_1)Wearable(Wearable).java
@@ -0,0 +1,9 @@
+//인터페이스는 설계도, 작업 지시서(반드시 만들어야 함. 안하면 에러가 나는 항목들)
+//public과 abstract 개념
+
+//다른곳에서 이 파일을 참조해야하기때문에 public을 쓴다
+public interface Wearable {
+
+  void putOn();//켜는 기능
+  void putOff();//끄는 기능
+}
